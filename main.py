da13bd57dd79d3e1e92a7ebca6b538f2643d8f17
ponpekepurin <furberpom@icloud.com>
2025-05-25 00:36:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,4 +36,5 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))  # ← RenderがPORTを環境変数で渡してくる
+    app.run(host="0.0.0.0", port=port)
